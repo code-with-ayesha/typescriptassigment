a3e0897c1b0387300d7096fe39c5008a42dc540f
code-with-ayesha <139987976+code-with-ayesha@users.noreply.github.com>
2023-09-15 18:13:15 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+// let myName = "Ayesha ijaz";
+// let todayDate = 29-7-2023 ;
+// store a person's name in a variable, and print a message to that person.
+console.log("Hello world");
+let name1;
+name1 = "Eric";
+console.log(`Hello ${name1}, would you like to study some python today?`);
---
+++ index.ts
@@ -0,0 +1,13 @@
+// let myName = "Ayesha ijaz";
+// let todayDate = 29-7-2023 ;
+
+// store a person's name in a variable, and print a message to that person.
+
+
+
+console.log("Hello world");
+
+let name1: string;
+name1 = "Eric";
+console.log(`Hello ${name1}, would you like to study some python today?`);
+ 
