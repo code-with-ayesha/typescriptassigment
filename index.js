469f8206f68f112d2011f6f3a61a2f37d1ef246a
code-with-ayesha <139987976+code-with-ayesha@users.noreply.github.com>
2023-09-15 18:21:43 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 "use strict";
-// let myName ="Ayesha Ijaz";
+// let myName = "Ayesha Ijaz";
 // let todayDate = 29-7-2023;
-// Print the person name with whitespaces.
-let personName = "\t      Ayesha Ijaz  \n";
-console.log("name with whitespaces");
-console.log(personName);
-// Print the person name without whitespaces.
-let strippedName = personName.trim();
-console.log("Name without whitespaces:");
-console.log(strippedName);
+// With addition,subtraction,multiplication and division operations 
+// that each result in number 8.
+// Addition
+let additionResult = 5 + 3;
+console.log("Addition:" + additionResult);
+// subtraction
+let subtractionResult = 10 - 2;
+console.log("Subtraction:" + subtractionResult);
+//  multiplication
+let multiplicationResult = 4 * 2;
+console.log("multiplication" + multiplicationResult);
+// division
+let divisionResult = 16 / 2;
+console.log("division" + divisionResult);
--- index.ts
+++ index.ts
@@ -1,22 +1,23 @@
 
-// let myName ="Ayesha Ijaz";
+// let myName = "Ayesha Ijaz";
 // let todayDate = 29-7-2023;
 
-// Print the person name with whitespaces.
-
- let personName: string = "\t      Ayesha Ijaz  \n";
-
- console.log("name with whitespaces");
- 
- console.log(personName);
- 
-// Print the person name without whitespaces.
-let strippedName = personName.trim();
-
-console.log("Name without whitespaces:");
-
-console.log(strippedName);
-
- 
+// With addition,subtraction,multiplication and division operations 
+// that each result in number 8.
 
 
+// Addition
+  let additionResult: number = 5 + 3;
+   console.log("Addition:"+ additionResult);
+
+// subtraction
+  let subtractionResult: number = 10-2;
+  console.log("Subtraction:" + subtractionResult);
+
+//  multiplication
+   let multiplicationResult: number = 4*2;
+   console.log("multiplication"+ multiplicationResult)
+
+// division
+ let divisionResult : number = 16 / 2;
+ console.log("division"+ divisionResult);
