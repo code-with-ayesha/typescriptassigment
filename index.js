e7c5c2e271d84beb1868e5c968f161de31ea8f27
code-with-ayesha <139987976+code-with-ayesha@users.noreply.github.com>
2023-09-15 18:17:17 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 "use strict";
-// let myName = "Ayesha ijaz";
-// let todayDate = 29-7-2023 ;
-// store a person's name in a variable, and print a message to that person.
-console.log("Hello world");
-let name1;
-name1 = "Eric";
-console.log(`Hello ${name1}, would you like to study some python today?`);
+let name1 = 'aYesHa iJaz';
+console.log(name1.toLowerCase());
+console.log(name1.toUpperCase());
+let titleCaseName = name1.toLowerCase().replace(/\b\w/g, (letter) => letter.toUpperCase());
+//  regex pattern
+console.log(titleCaseName);
--- index.ts
+++ index.ts
@@ -1,13 +1,10 @@
-// let myName = "Ayesha ijaz";
-// let todayDate = 29-7-2023 ;
 
-// store a person's name in a variable, and print a message to that person.
+let name1: string = 'aYesHa iJaz';
+console.log(name1.toLowerCase());
 
+console.log(name1.toUpperCase());
 
-
-console.log("Hello world");
-
-let name1: string;
-name1 = "Eric";
-console.log(`Hello ${name1}, would you like to study some python today?`);
- 
+let titleCaseName: string = name1.toLowerCase().replace(/\b\w/g,
+ (letter) => letter.toUpperCase());
+//  regex pattern
+console.log( titleCaseName);
