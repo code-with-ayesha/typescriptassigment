b4b66da1fa2a4608354b901db450ff08c8c20c51
code-with-ayesha <139987976+code-with-ayesha@users.noreply.github.com>
2023-09-15 18:20:47 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 "use strict";
-// let myName = "Ayesha Ijaz";
+// let myName ="Ayesha Ijaz";
 // let todayDate = 29-7-2023;
-// Print the famous person name and his qoute in a message called variable.
-let qoute1 = "In the middle of every difficulty lies opportunity";
-let famous_person = "Albert Einstein";
-let message = `${famous_person} once said, "${qoute1}"`;
-console.log(message);
+// Print the person name with whitespaces.
+let personName = "\t      Ayesha Ijaz  \n";
+console.log("name with whitespaces");
+console.log(personName);
+// Print the person name without whitespaces.
+let strippedName = personName.trim();
+console.log("Name without whitespaces:");
+console.log(strippedName);
--- index.ts
+++ index.ts
@@ -1,15 +1,22 @@
 
-// let myName = "Ayesha Ijaz";
-
+// let myName ="Ayesha Ijaz";
 // let todayDate = 29-7-2023;
 
-// Print the famous person name and his qoute in a message called variable.
+// Print the person name with whitespaces.
 
+ let personName: string = "\t      Ayesha Ijaz  \n";
 
-let qoute1: string = "In the middle of every difficulty lies opportunity";
-let famous_person: string = "Albert Einstein";
-let message: string = `${famous_person} once said, "${qoute1}"`;
-console.log(message);
+ console.log("name with whitespaces");
+ 
+ console.log(personName);
+ 
+// Print the person name without whitespaces.
+let strippedName = personName.trim();
 
+console.log("Name without whitespaces:");
+
+console.log(strippedName);
+
+ 
 
 
