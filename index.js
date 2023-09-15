06fc4d5200ff9824e60fde8475e177189dd3ab1c
code-with-ayesha <139987976+code-with-ayesha@users.noreply.github.com>
2023-09-15 18:19:35 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 "use strict";
-let qoute = "In the middle of every difficulty lies opportunity.";
-let author = "Albert Einstein";
-console.log(`${author} once said, "${qoute}"`);
+// let myName = "Ayesha Ijaz";
+// let todayDate = 29-7-2023;
+// Print the famous person name and his qoute in a message called variable.
+let qoute1 = "In the middle of every difficulty lies opportunity";
+let famous_person = "Albert Einstein";
+let message = `${famous_person} once said, "${qoute1}"`;
+console.log(message);
--- index.ts
+++ index.ts
@@ -1,3 +1,15 @@
-let qoute: string = "In the middle of every difficulty lies opportunity.";
-let author: string = "Albert Einstein";
-console.log(`${author} once said, "${qoute}"`);
+
+// let myName = "Ayesha Ijaz";
+
+// let todayDate = 29-7-2023;
+
+// Print the famous person name and his qoute in a message called variable.
+
+
+let qoute1: string = "In the middle of every difficulty lies opportunity";
+let famous_person: string = "Albert Einstein";
+let message: string = `${famous_person} once said, "${qoute1}"`;
+console.log(message);
+
+
+
